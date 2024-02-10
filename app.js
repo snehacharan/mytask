614dd6e240830a445c21fab60029fe24bd7acca2
snehacharan <reddemsnehacharanreddy@gmail.com>
2024-02-10 13:29:23 +0530

    second

---
+++ app.js
@@ -0,0 +1,29 @@
+function mike(checkbox) {
+    if (checkbox.checked) {
+        task.style.color= "gray"; 
+    } else {
+      task.style.color ="navy"; 
+    }
+  }
+function mike2(checkbox) {
+   if (checkbox.checked) {
+       bask.style.color= "gray"; 
+   } else {
+     bask.style.color ="navy";
+   }
+ }
+function mike3(checkbox) {
+ if (checkbox.checked) {
+     ask.style.color= "gray"; 
+ } else {
+     ask.style.color ="navy"; 
+ }
+}
+function mike4(checkbox) {
+ if (checkbox.checked) {
+     tas.style.color= "gray";
+ } else {
+   tas.style.color ="navy"; 
+ }
+}
+  
